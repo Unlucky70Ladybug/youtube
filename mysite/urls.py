6c3cfa81d386bd959da008fb5b79e93ea44b07ab
Unlucky70Ladybug <unhappywrld417@gmail.com>
2023-12-21 20:39:13 +0900

    path name erase

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('youtube/',include('youtube.urls')),
+    path('',include('youtube.urls')),
 ]

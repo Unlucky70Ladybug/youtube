d06788631512b811a09fda5ec5315dbbb9c1cdf9
Unlucky70Ladybug <unhappywrld417@gmail.com>
2023-12-21 17:17:14 +0900

    DEBUG use env

--- mysite/settings/locals.py
+++ mysite/settings/locals.py
@@ -33,10 +33,10 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
